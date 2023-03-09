1e7c9188673ee5905dad06e599d176e1fabd016c
Zorous <oussamabelhadi5@gmail.com>
2023-03-09 01:13:53 +0100

    my configs

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Oussama Belhadi',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'My Personal Blog',
 
   // social usernames (optional)
   twitter: '#',
